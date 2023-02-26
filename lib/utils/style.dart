fcb1515872dbb9f6a191ac1b595c73e18117e7de
HamzaNechi <hamza.nechi@gmail.com>
2023-02-26 13:01:16 +0100

    interfaces welcome & authentification

--- lib/utils/style.dart
+++ lib/utils/style.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 
 class Style {
   /// Colors palette
-  static const Color primaryColor = Color(0xFF92B4EC); //#92B4EC
-  static const Color secondaryColor = Color(0xFFFFE69A); //#FFE69A
-  static const Color secondaryDark = Color(0xFFFFD24C); //#FFD24C
-  static const Color blackColor = Color(0xFF151515); // #151515 black
+  static const Color primary=Color(0xFF263171); //#263171 p
+  static const Color primaryLight=Color(0xFF3862c8); //#3862c8 p2
+  static const Color second=Color(0xFF9aa1cb); //#9aa1cb p3
+  static const Color secondLight=Color(0xFFcbcedf); //#cbcedf p4
+  static const Color clair=Color(0xFFe0e8fd);//#e0e8fd
+  static const Color blackColor = Color(0xFF292930); // #292930 black
   static const Color whiteColor = Color(0xFFFFFFFF); //white
 }
